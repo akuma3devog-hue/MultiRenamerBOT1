dca9bf2fe6dbca401ce62b7bef6301ccf07e4a5a
akuma3devog-hue <akuma3devog@gmail.com>
2026-01-15 21:48:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import threading
+import telebot
+
+from config import BOT_TOKEN, PORT
+from server import app
+from start import register_start
+
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")
+
+register_start(bot)
+
+def run_web():
+    app.run(host="0.0.0.0", port=PORT)
+
+def run_bot():
+    print("🤖 Bot started polling...")
+    bot.infinity_polling(skip_pending=True)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_web).start()
+    run_bot()
