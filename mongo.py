d033cb3978c106a59e92b8e197995e490e7babf4
akuma3devog-hue <akuma3devog@gmail.com>
2026-01-15 22:06:23 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -7,6 +7,8 @@ client = MongoClient(MONGO_URI)
 db = client["batch_renamer"]
 users = db["users"]
 
+# -------- USER / BATCH --------
+
 def reset_user(user_id):
     users.delete_one({"user_id": user_id})
 
@@ -21,4 +23,27 @@ def create_user(user_id):
             "change_file_id": False
         }},
         upsert=True
+    )
+
+def get_user(user_id):
+    return users.find_one({"user_id": user_id})
+
+# -------- FILE HANDLING --------
+
+def add_file(user_id, file_data):
+    users.update_one(
+        {"user_id": user_id},
+        {"$push": {"files": file_data}}
+    )
+
+def get_files(user_id):
+    user = get_user(user_id)
+    return user["files"] if user else []
+
+# -------- RENAME --------
+
+def set_rename(user_id, rename_data):
+    users.update_one(
+        {"user_id": user_id},
+        {"$set": {"rename": rename_data}}
     )
