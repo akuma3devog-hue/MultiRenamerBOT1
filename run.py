a52d97171fda16acb15af8c18babe47c2b50b9b5
akuma3devog-hue <akuma3devog@gmail.com>
2026-01-15 17:25:09 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Force project root into PYTHONPATH
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
 from main import *
