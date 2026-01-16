b1944b3c5d7417da65451f80a0c5ecfd8b478302
akuma3devog-hue <akuma3devog@gmail.com>
2026-01-16 22:08:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+from handlers import register_handlers
+
+app = Client(
+    "renamer_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+register_handlers(app)
+
+print("🤖 Bot running...")
+app.run()
