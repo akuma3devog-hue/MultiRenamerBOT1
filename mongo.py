d363e59672fa73a044b82673d319808742af69fb
akuma3devog-hue <akuma3devog@gmail.com>
2026-01-17 07:11:53 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -6,10 +6,6 @@ client = MongoClient(os.getenv("MONGO_URI"))
 db = client["multi_renamer"]
 users = db["users"]
 
-# -----------------------------
-# USER / BATCH
-# -----------------------------
-
 def reset_user(user_id):
     users.delete_one({"user_id": user_id})
 
@@ -20,12 +16,15 @@ def create_user(user_id):
             "user_id": user_id,
             "files": [],
             "rename": None,
-            "thumbnail": None,          # ✅ ADD: per-user thumbnail
-            "processing": False,
+            "thumbnail": None,
+            "awaiting_thumbnail": False,
             "created_at": datetime.utcnow()
         }},
         upsert=True
     )
+
+def add_file(user_id, file):
+    users.update_one({"user_id": user_id}, {"$push": {"files": file}})
 
 def get_user(user_id):
     return users.find_one({"user_id": user_id})
@@ -34,32 +33,20 @@ def get_files(user_id):
     user = get_user(user_id)
     return user["files"] if user else []
 
-# -----------------------------
-# FILES
-# -----------------------------
-
-def add_file(user_id, file):
-    users.update_one(
-        {"user_id": user_id},
-        {"$push": {"files": file}}
-    )
-
-# -----------------------------
-# THUMBNAIL (NEW)
-# -----------------------------
-
+# ---------- THUMBNAIL ----------
 def set_thumbnail(user_id, file_id):
-    users.update_one(
-        {"user_id": user_id},
-        {"$set": {"thumbnail": file_id}}
-    )
+    users.update_one({"user_id": user_id}, {"$set": {"thumbnail": file_id}})
 
 def get_thumbnail(user_id):
     user = get_user(user_id)
     return user.get("thumbnail") if user else None
 
 def delete_thumbnail(user_id):
-    users.update_one(
-        {"user_id": user_id},
-        {"$set": {"thumbnail": None}}
-    )
+    users.update_one({"user_id": user_id}, {"$set": {"thumbnail": None}})
+
+def set_awaiting_thumb(user_id, state: bool):
+    users.update_one({"user_id": user_id}, {"$set": {"awaiting_thumbnail": state}})
+
+def is_awaiting_thumb(user_id):
+    user = get_user(user_id)
+    return user.get("awaiting_thumbnail", False) if user else False
